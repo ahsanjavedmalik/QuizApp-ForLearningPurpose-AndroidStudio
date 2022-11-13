94c27a33bb91561996a7f553bd93e097a80870da
AHSAN JAVED MALIK <bcsf19a004@pucit.edu.pk>
2022-11-13 20:27:37 +0500

    java class for 6th kalma

--- app/src/main/java/com/example/quiz/Sixth_Kalma.java
+++ app/src/main/java/com/example/quiz/Sixth_Kalma.java
@@ -1,4 +1,14 @@
 package com.example.quiz;
 
-public class Sixth_Kalma {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sixth_Kalma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sixth_kalma);
+    }
 }
