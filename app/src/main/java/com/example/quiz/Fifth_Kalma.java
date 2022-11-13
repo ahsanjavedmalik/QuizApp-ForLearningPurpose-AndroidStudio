a701c2c83c45d9403e12fb400cde87dc256faf4b
AHSAN JAVED MALIK <bcsf19a004@pucit.edu.pk>
2022-11-13 20:28:04 +0500

    java class for 5th kalma

--- app/src/main/java/com/example/quiz/Fifth_Kalma.java
+++ app/src/main/java/com/example/quiz/Fifth_Kalma.java
@@ -1,4 +1,14 @@
 package com.example.quiz;
 
-public class Fifth_Kalma {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fifth_Kalma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fifth_kalma);
+    }
 }
