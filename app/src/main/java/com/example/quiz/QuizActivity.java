10eb1b3a8003cc0413abba427ca163cc1b5a545f
AHSAN JAVED MALIK <bcsf19a004@pucit.edu.pk>
2022-11-13 20:00:18 +0500

    Implemted the function which will randomly chose a question: function name NextQuestion

--- app/src/main/java/com/example/quiz/QuizActivity.java
+++ app/src/main/java/com/example/quiz/QuizActivity.java
@@ -25,12 +25,12 @@ public class QuizActivity extends AppCompatActivity {
         opt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(quesNo == 3){
+                if (quesNo == 3) {
                     Toast.makeText(QuizActivity.this, "CORRECT! Score= +10", Toast.LENGTH_SHORT).show();
-                    score = score+10;
+                    score = score + 10;
 
                 }
-                quesNo = quesNo+1;
+                quesNo = quesNo + 1;
                 NextQuestion();
             }
         });
@@ -38,11 +38,11 @@ public class QuizActivity extends AppCompatActivity {
         opt2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(quesNo == 1){
+                if (quesNo == 1) {
                     Toast.makeText(QuizActivity.this, "CORRECT! Score= +10", Toast.LENGTH_SHORT).show();
-                    score = score+10;
+                    score = score + 10;
                 }
-                quesNo = quesNo+1;
+                quesNo = quesNo + 1;
                 NextQuestion();
             }
         });
@@ -50,11 +50,11 @@ public class QuizActivity extends AppCompatActivity {
         opt3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(quesNo == 5){
+                if (quesNo == 5) {
                     Toast.makeText(QuizActivity.this, "CORRECT! Score= +10", Toast.LENGTH_SHORT).show();
-                    score = score+10;
+                    score = score + 10;
                 }
-                quesNo = quesNo+1;
+                quesNo = quesNo + 1;
                 NextQuestion();
             }
         });
@@ -63,14 +63,55 @@ public class QuizActivity extends AppCompatActivity {
         opt4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(quesNo == 2 || quesNo == 4){
+                if (quesNo == 2 || quesNo == 4) {
                     Toast.makeText(QuizActivity.this, "CORRECT! Score= +10", Toast.LENGTH_SHORT).show();
-                    score = score+10;
+                    score = score + 10;
 
                 }
-                quesNo = quesNo+1;
+                quesNo = quesNo + 1;
                 NextQuestion();
             }
         });
+    }
 
+    private void NextQuestion()
+    {
+        if(quesNo==2){
+            question.setText(" اللہ پاک سے استغفار کس کلمہ میں مانگی گی ہے؟");
+            opt1.setText("رد کفر");
+            opt2.setText("طیبہ");
+            opt3.setText("توحید");
+            opt4.setText("استغفار");
+        }
+        if(quesNo==3){
+            question.setText("رد کفر کونسا کلمہ ہے؟");
+            opt1.setText("چھٹا");
+            opt2.setText("پہلا");
+            opt3.setText("تیسرا");
+            opt4.setText("دوسرا");
+        }
+
+        if(quesNo==4){
+            question.setText("کلمہ شہادت کونسا کلمہ ہے؟");
+            opt1.setText("چھٹا");
+            opt2.setText("پہلا");
+            opt3.setText("تیسرا");
+            opt4.setText("دوسرا");
+
+        }
+        if(quesNo==5){
+            question.setText("کلموں کی تعداد کتنی ہے؟");
+            opt1.setText("ایک");
+            opt2.setText("چار");
+            opt3.setText("چھے");
+            opt4.setText("پانچ");
+        }
+        if(quesNo==6){
+            question.setText("Thank You So Much! Your Total Score was: " + score);
+            opt1.setVisibility(View.GONE);
+            opt2.setVisibility(View.GONE);
+            opt3.setVisibility(View.GONE);
+            opt4.setVisibility(View.GONE);
+        }
+    }
 }
