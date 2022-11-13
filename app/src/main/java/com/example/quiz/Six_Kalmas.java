0ae2d51e3dbe26286b765af39dd0f911f2608a83
AHSAN JAVED MALIK <bcsf19a004@pucit.edu.pk>
2022-11-13 20:14:08 +0500

    function for first kalma

--- app/src/main/java/com/example/quiz/Six_Kalmas.java
+++ app/src/main/java/com/example/quiz/Six_Kalmas.java
@@ -1,4 +1,18 @@
 package com.example.quiz;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class Six_Kalmas {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+public class Six_Kalmas extends  AppCompatActivity{
+    Button first,second,third,fourth,fifth,sixth;
+
+
+
+    private void changeActivity1()
+    {
+        Intent intent = new Intent(this,First_Kalma.class);
+        startActivity(intent);
+    }
 }
