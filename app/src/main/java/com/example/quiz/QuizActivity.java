ed6958cb2f5241fbf7665167de3ecce1b0ae7572
AHSAN JAVED MALIK <bcsf19a004@pucit.edu.pk>
2022-11-13 19:56:39 +0500

    option 1  logic implemented

--- app/src/main/java/com/example/quiz/QuizActivity.java
+++ app/src/main/java/com/example/quiz/QuizActivity.java
@@ -1,4 +1,38 @@
 package com.example.quiz;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class QuizActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+    Button opt1,opt2,opt3,opt4;
+    TextView question;
+    int score=0,quesNo=1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+        question = findViewById(R.id.question);
+        opt1 = findViewById(R.id.option1);
+        opt2 = findViewById(R.id.option2);
+        opt3 = findViewById(R.id.option3);
+        opt4 = findViewById(R.id.option4);
+
+        opt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(quesNo == 3){
+                    Toast.makeText(QuizActivity.this, "CORRECT! Score= +10", Toast.LENGTH_SHORT).show();
+                    score = score+10;
+
+                }
+                quesNo = quesNo+1;
+                NextQuestion();
+            }
+        });
+
 }
